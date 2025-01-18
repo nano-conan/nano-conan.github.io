9aef95b4bbdee7241d2c11113aaab86152edbe54
Conan <conanjay@icloud.com>
2025-01-18 13:49:42 -0400

    mobile support

--- script.js
+++ script.js
@@ -2,27 +2,34 @@ const herosDraggable = document.querySelectorAll('.hero');
 let currentDroppable = null;
 
 herosDraggable.forEach((hero) => {
-  hero.onmousedown = function(event) {
+  const startDrag = (event) => {
+    // For mobile/touch, we use touch events, otherwise mouse events
+    const clientX = event.clientX || event.touches[0].clientX;
+    const clientY = event.clientY || event.touches[0].clientY;
 
-    let shiftX = event.clientX - hero.getBoundingClientRect().left;
-    let shiftY = event.clientY - hero.getBoundingClientRect().top;
+    let shiftX = clientX - hero.getBoundingClientRect().left;
+    let shiftY = clientY - hero.getBoundingClientRect().top;
 
     hero.style.position = 'absolute';
     hero.style.zIndex = 1000;
     document.body.append(hero);
 
-    moveAt(event.pageX, event.pageY);
+    moveAt(clientX, clientY);
 
     function moveAt(pageX, pageY) {
       hero.style.left = pageX - shiftX + 'px';
       hero.style.top = pageY - shiftY + 'px';
     }
 
-    function onMouseMove(event) {
-      moveAt(event.pageX, event.pageY);
+    function onMove(event) {
+      // For mobile/touch, use touch coordinates
+      const pageX = event.pageX || event.touches[0].pageX;
+      const pageY = event.pageY || event.touches[0].pageY;
+
+      moveAt(pageX, pageY);
 
       hero.hidden = true;
-      let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+      let elemBelow = document.elementFromPoint(event.clientX || event.touches[0].clientX, event.clientY || event.touches[0].clientY);
       hero.hidden = false;
 
       if (!elemBelow) return;
@@ -39,15 +46,28 @@ herosDraggable.forEach((hero) => {
       }
     }
 
-    document.addEventListener('mousemove', onMouseMove);
-
-    hero.onmouseup = function() {
-      document.removeEventListener('mousemove', onMouseMove);
-      hero.onmouseup = null;
+    // Add appropriate event listeners for mouse or touch
+    const onEnd = (endEvent) => {
+      document.removeEventListener('mousemove', onMove);
+      document.removeEventListener('touchmove', onMove);
+      document.removeEventListener('mouseup', onEnd);
+      document.removeEventListener('touchend', onEnd);
     };
 
+    // Listen to mouse/touchmove events
+    document.addEventListener('mousemove', onMove);
+    document.addEventListener('touchmove', onMove);
+
+    // Listen for mouse/touch end
+    document.addEventListener('mouseup', onEnd);
+    document.addEventListener('touchend', onEnd);
   };
 
+  // Use touchstart or mousedown depending on the input type
+  hero.addEventListener('mousedown', startDrag);
+  hero.addEventListener('touchstart', startDrag);
+
+  // Prevent default drag behavior
   hero.ondragstart = function() {
     return false;
   };
@@ -60,3 +80,4 @@ function enterDroppable(elem) {
 function leaveDroppable(elem) {
   elem.style.background = '';
 }
+
